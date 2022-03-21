7eb914a056b39faec8e8cbaa8ff3716d63314422
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-03-21 13:47:40 -0600

    Refactor TodoContainer using functions

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,92 +1,91 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Header from "./Header"
 import InputTodo from "./InputTodo"
-import TodosList from "./TodosList";
-import { v4 as uuidv4 } from "uuid";
+import TodosList from "./TodosList"
+import { v4 as uuidv4 } from "uuid"
 
-class TodoContainer extends React.Component {
-
-  state = {
-    todos: [
-      {
-        id: uuidv4(),
-        title: "Setup development environment",
-        completed: true
-      },
-      {
-        id: uuidv4(),
-        title: "Develop website and add content",
-        completed: false
-      },
-      {
-        id: uuidv4(),
-        title: "Deploy to live server",
-        completed: false
-      }
-    ]
-  };
-
-  handleChange = id => {
-    this.setState(prevState => ({    
-      todos: prevState.todos.map((todo) => {     
-        if (todo.id === id) {  
-          return {
-            ...todo, completed: !todo.completed
-          }      
-        }      
-        return todo;    
-      })  
-    }));    
-  };
-
-  delTodo = id => {
-    this.setState({    
-      todos: [      
-        ...this.state.todos.filter(todo => {        
-          return todo.id !== id;      
-        })    
-      ]  
-    });
-  };
-
-  addTodoItem = title => {
-    const newTodo = {    
-      id: uuidv4(),    
-      title: title,    
-      completed: false  
-    };  
-    this.setState({    
-      todos: [...this.state.todos, newTodo]  
-    });
-  };
-
+const TodoContainer = () => {
+  const getInitialTodos = () => {
+    // getting stored items
+    const temp = localStorage.getItem("todos")
+    const savedTodos = JSON.parse(temp)
+    return savedTodos || []
+  }
   
-  setUpdate = (updatedTitle, id) => {
-    this.setState({
-      todos: this.state.todos.map((todo) => {
-        if(todo.id === id) {
+  const [todos, setTodos] = useState(getInitialTodos())
+
+  const handleChange = id => {
+    setTodos(prevState =>
+      prevState.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          }
+        }
+        return todo
+      })
+    )
+  }
+
+  const delTodo = id => {
+    setTodos([
+      ...todos.filter(todo => {
+        return todo.id !== id
+      }),
+    ])
+  }
+
+  const addTodoItem = title => {
+    const newTodo = {
+      id: uuidv4(),
+      title: title,
+      completed: false,
+    }
+    setTodos([...todos, newTodo])
+  }
+
+  const setUpdate = (updatedTitle, id) => {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
           todo.title = updatedTitle
         }
         return todo
       })
-    })
+    )
   }
 
-  render() {
-    return (
-      <div className="container">
-        <div className="inner">
-          <Header />
-          <InputTodo addTodoProps={this.addTodoItem} />
-          <TodosList 
-            todos={this.state.todos} 
-            handleChangeProps={this.handleChange} 
-            deleteTodoProps={this.delTodo}
-            setUpdate ={this.setUpdate} 
-          />
-        </div>
+  useEffect(() => {
+    // getting stored items
+    const temp = localStorage.getItem("todos")
+    const loadedTodos = JSON.parse(temp)
+  
+    if (loadedTodos) {
+      setTodos(loadedTodos)
+    }
+  }, [])
+
+  useEffect(() => {
+    // storing todos items
+    const temp = JSON.stringify(todos)
+    localStorage.setItem("todos", temp)
+  }, [todos])
+
+  return (
+    <div className="container">
+      <div className="inner">
+        <Header />
+        <InputTodo addTodoProps={addTodoItem} />
+        <TodosList
+          todos={todos}
+          handleChangeProps={handleChange}
+          deleteTodoProps={delTodo}
+          setUpdate={setUpdate}
+        />
       </div>
-    );
-  }
+    </div>
+  )
 }
+
 export default TodoContainer
