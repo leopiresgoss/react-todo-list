384e5ff9838d5b2e8aa5cf7cad121049e7223cbe
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-03-21 13:17:05 -0600

    Add class-based version

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './App.css';
+import TodoContainer from "./components/TodoContainer"
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TodoContainer />
   </React.StrictMode>,
   document.getElementById('root')
 );
